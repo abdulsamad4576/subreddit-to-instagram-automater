612fbe35f7101ebd2dd9a93a1d83287d0e9f1a36
abdulsamad4576 <152664995+abdulsamad4576@users.noreply.github.com>
2023-12-03 03:58:20 +0500

    Add files via upload

---
+++ autogram.py
@@ -0,0 +1,62 @@
+import praw
+import os
+import requests
+from instagrapi import Client
+from datetime import date
+import subprocess
+
+def main():
+    top_posts = redditAPI()
+    for post in top_posts:
+        if post.url.endswith('.jpg') or post.url.endswith('.png'):
+            extension = '.jpg' if post.url.endswith('.jpg') else '.png'
+            isDownloaded, image_path = download_image(post.url, extension)
+            if isDownloaded:
+                upload_to_instagram(image_path)
+                break
+
+def redditAPI():
+    reddit = praw.Reddit(client_id=os.environ.get('REDDIT_CLIENT_ID'),
+                         client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
+                         user_agent='topAPI')
+    subreddit = reddit.subreddit('memes')
+    return subreddit.top(time_filter='day', limit=20)
+
+def download_image(url, extension):
+    response = requests.get(url)
+    if response.status_code == 200:
+        image_name = get_image_name()
+        # Save images within the repository
+        image_path = f'./images/{image_name}{extension}'
+        with open(image_path, 'wb') as file:
+            file.write(response.content)
+        increment_image_name()
+        # Push the image back to the repository
+        commit_and_push(image_name + extension)
+        return True, image_path
+    return False, None
+
+def commit_and_push(filename):
+    subprocess.run(['git', 'add', f'./images/{filename}'])
+    subprocess.run(['git', 'commit', '-m', f'Add image {filename}'])
+    subprocess.run(['git', 'push'])
+
+def upload_to_instagram(image_path):
+    insta = Client()
+    insta.login(os.environ.get('INSTA_USERNAME'), os.environ.get('INSTA_PASSWORD'))
+    caption = str(date.today())
+    insta.photo_upload(image_path, caption)
+
+def get_image_name():
+    with open(f'./image_counter.txt', 'r') as file:
+        number = int(file.read().strip())
+    return f'image_{number}'
+
+def increment_image_name():
+    with open(f'./image_counter.txt', 'r') as file:
+        number = int(file.read().strip())
+    with open(f'./image_counter.txt', 'w') as file:
+        file.write(str(number + 1))
+
+if __name__ == '__main__':
+    main()
